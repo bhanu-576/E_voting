fc0c40d23f196cb97a29a9dfd2b0a96870facba9
bhanu <bhanusharma576@gmail.com>
2021-01-18 20:23:53 +0530

    first comit

---
+++ dbcon.php
@@ -0,0 +1,28 @@
+<?php 
+
+session_start();
+$con = mysqli_connect('localhost','root');
+if($con){
+   echo "successful";
+}else{
+    echo "mc";
+}
+
+
+mysqli_select_db($con,'register');
+$name =$_POST['name'];
+$enrollment= $_POST['enrollment'];
+$party = $_POST['party'];
+
+$q = "select * from register where name = '$name' && enrollment = '$enrollment' && partyname = '$party' ";
+$result = mysqli_query($con,$q);
+$num = mysqli_num_rows($result);
+
+if($num == 1){
+    echo "already registered";
+}else{
+    $qy = "insert into register(name , enrollment , partyname) values('$name' ,'$enrollment' , '$party')";
+    mysqli_query($con , $qy);
+    echo " saved";
+}
+?>
